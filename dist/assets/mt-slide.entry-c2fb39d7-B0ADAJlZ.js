9a3fe57f4b0b9039e2ed99cdf6427570972ace56
Kumar Gaurav <kumar.gaurav@monotype.com>
2024-08-04 16:57:53 +0530

    dist updated

---
+++ dist/assets/mt-slide.entry-c2fb39d7-B0ADAJlZ.js
@@ -0,0 +1,4 @@
+import{G as e,v as s}from"./index-D230z0wX.js";/*!
+ * Built with Stencil
+ * Copyright (c) 2024 Monotype Imaging Inc..
+ */const d=":host{display:block;flex:0 0 100%;width:100%}",c=d,a=class{constructor(t){e(this,t)}render(){return s("div",{key:"8e51780044dc0d0871f77336f6d9ceb83e216b4a",class:"mt-slide",part:"mt-slide"},s("slot",{key:"35b741f1cbc9a2ac1b56f4d0966b44b04b64d392"}))}};a.style=c;export{a as mt_slide};
